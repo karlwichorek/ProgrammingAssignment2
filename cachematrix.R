b26113107bc77ee3575dabc39adeae3e8c83336e
Karl Wichorek <wichorek88@gmail.com>
2015-07-25 19:39:34 -0400

    Edited the cachematrix file for the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The following functions work together to create a square invertible matrix
+# and make the inverse of the matrix available in the cache environment
 
-## Write a short comment describing this function
-
+# makeCacheMatrix defines and returns the list of functions
+# used by cacheSolve to get or set the inverted matrix in cache
 makeCacheMatrix <- function(x = matrix()) {
-
+  # clear cache
+  cache <- NULL
+  
+  # create the matrix in the working environment
+  set <- function(y) {
+    x <<- y
+    cache <<- NULL
+  }
+  
+  # get value of the matrix
+  get <- function() x
+  
+  # invert matrix and store result in cache
+  setMatrix <- function(inverse) cache <<- inverse
+  
+  # get inverted matrix from cache
+  getInverse <- function() cache
+  
+  # push functions to working environment
+  list(set = set, get = get,
+       setMatrix = setMatrix,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+# cacheSolve calcluates the inverse of the matrix created in makeCacheMatrix
+# If the inverted matrix does not exist in cache,
+# it it created in the working environment 
+# and it's inverted value is stored in cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # Fetch the cached value for the inverse
+  m <- x$getInverse() 
+  
+  # If the cache is not empty, return cache
+  if(!is.null(m)) { 
+    message("getting cached data")
+    return(m)
+  }
+  
+  # If the cache was empty, calculate the inverse, store it in the cache, 
+  # and then return the result
+  
+  # Get value of matrix
+  data <- x$get()
+  
+  # Calculate the inverse of the matrix
+  m <- solve(data)
+  
+  # Cache the result
+  x$setInverse(m)
+  
+  # Return the inverse
+  return(m)
 }
